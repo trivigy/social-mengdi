377deb191d6078975fe78005a1e76d6cf9c606ca
Mengdi Lin <ml3567@columbia.edu>
2014-10-17 17:58:08 -0400

    server listens to one client

--- client.py
+++ client.py
@@ -1,2 +1,13 @@
+# Echo client program
+import socket
+
+HOST = '127.0.0.1'
+PORT = 50030;            # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall('Hello, worldlololl fjkdlsjlk ')
+data = s.recv(1024)
+s.close()
+print 'Received', repr(data)
 if __name__ == '__main__':
     print "CLIENT WORKS!!!"
--- server.py
+++ server.py
@@ -1,2 +1,22 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 50020;
+
+def server_main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    print 'Connected by', addr
+    while True:
+        data = conn.recv(1024)
+        print data
+        if (data):
+            conn.sendall(data)
+        if (data == "exit"):
+            conn.close()
+
+#127.0.0.1
 if __name__ == '__main__':
-    print "SERVER WORKS!!!"
+    server_main()
