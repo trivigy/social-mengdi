67d78189393f41ae39d9bc7addf3267bc0a1e952
Konstantin Itskov <konstantin.itskov@kovits.com>
2014-10-17 18:00:43 -0400

    implements client program.

--- client.py
+++ client.py
@@ -1,2 +1,45 @@
+import socket
+import cmd
+
+class Main(cmd.Cmd):
+    def preloop(self):
+        self.prompt = "#-> "
+        self.HOST = '127.0.0.1'
+        self.PORT = 50020
+        
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.HOST, self.PORT))
+
+    def do_send(self, msg):
+        self.socket.sendall(msg)
+        data = self.socket.recv(1024)
+        if data:
+            print data
+        
+        
+    def do_exit(self, line):
+        self.socket.close()
+        return True
+
+
 if __name__ == '__main__':
-    print "CLIENT WORKS!!!"
+    main = Main()
+    try:
+        main.cmdloop()
+    except KeyboardInterrupt:
+        main.echo("")
+        main.onecmd("exit")
+
+# def main():
+#     
+#     
+#     
+#     totalsent = 0
+#     while totalsent < len(msg):
+#         sent = self.sock.send(msg[totalsent:])
+#         if sent == 0:
+#             raise RuntimeError("socket connection broken")
+#         totalsent = totalsent + sent
+# 
+# if __name__ == '__main__':
+#     main()
