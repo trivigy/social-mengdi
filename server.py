1634fd0f272545327159104003e24d606b17f204
Konstantin Itskov <konstantin.itskov@kovits.com>
2014-10-17 20:33:30 -0400

    fixing the server exit command

--- server.py
+++ server.py
@@ -4,42 +4,44 @@ import threading
 HOST = '127.0.0.1'
 PORT = 50030
 
-end_server = False
 class serverThread(threading.Thread):
-    def __init__(self,conn,addr,thread_list):
+    def __init__(self,conn,addr,thread_list, s_server):
         threading.Thread.__init__(self)
         self.conn = conn
         self.addr = addr
+        self.s_server = s_server
         thread_list.append(self)
         self.thread_list = thread_list
+        self.kill = False
 
     def run(self):
-        while True:
-            print "here"
+        while not self.kill:
             data = self.conn.recv(1024)
             print data
             if (data):
                 for element in self.thread_list:
                     element.conn.sendall(data)
                 if (data == "exit"):
-                    end_server = True
-                    print "inside here"
-                    break
+                    for element in self.thread_list:
+                        element.kill = True
+                    self.s_server.shutdown(socket.SHUT_RDWR)
 
 def server_main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
     s.listen(5)
     thread_list = []
+    
     while True:
-        conn,addr=s.accept()
-        print addr
-        if (conn):
-            thread = serverThread(conn,addr,thread_list)
-            thread.start()
-        if (end_server):
-            s.close()
-            return
+        print "HAZA"
+        try:
+            conn, addr = s.accept()
+            print addr
+            if (conn):
+                thread = serverThread(conn, addr, thread_list, s)
+                thread.start()
+        except:
+            break
         
 
 if __name__ == '__main__':
